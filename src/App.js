84c8300bf16aaad5057c2892e9f058eafb98d05d
Jean Celin <jean_estudos@hotmail.com>
2024-04-24 12:04:18 -0300

    criado tabela base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
 
+const tabelaIMC=()=>{
+  return(
+    <table border='1' style={{borderCollapse:'collapse'}}>
+      <thead>
+        <tr>
+          <th>
+            Classificação
+          </th>
+          <th>
+            IMC
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            Abaixo
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    
+    </>
+  )
 }
 
 export default App;
